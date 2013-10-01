e6bc102bcc9759a1b9573d9bbc4014d20773e0b2
Quentin Bahers <quentin.bahers@gmail.com>
2013-10-01 15:15:33 +0200

    Add a full_title helper

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -9,9 +9,14 @@ describe "Static pages" do
       expect(page).to have_content('Sensor Cloud App')
     end
 
-    it "should have the title 'Home'" do
+    it "should have the base title" do
       visit '/static_pages/home'
-      expect(page).to have_title("Sensor Cloud App | Home")
+      expect(page).to have_title("Sensor Cloud App")
+    end
+
+    it "should not have a custom page title" do
+      visit '/static_pages/home'
+      expect(page).not_to have_title('| Home')
     end
   end
 
