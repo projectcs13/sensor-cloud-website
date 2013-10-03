f188d1185dc7fc0e05a10232fd616cbfe95a53ef
Alberto Blazquez <hola@albertoblazquez.net>
2013-10-03 16:05:58 +0200

    Redirection after creation/edition is done

--- app/controllers/resources_controller.rb
+++ app/controllers/resources_controller.rb
@@ -28,7 +28,7 @@ class ResourcesController < ApplicationController
 
     respond_to do |format|
       if @resource.save
-        format.html { redirect_to @resource, notice: 'Resource was successfully created.' }
+        format.html { redirect_to action: 'index', status: :moved_permanently }
         format.json { render action: 'show', status: :created, location: @resource }
       else
         format.html { render action: 'new' }
@@ -42,7 +42,7 @@ class ResourcesController < ApplicationController
   def update
     respond_to do |format|
       if @resource.update(resource_params)
-        format.html { redirect_to @resource, notice: 'Resource was successfully updated.' }
+        format.html { redirect_to action: 'index', status: :moved_permanently }
         format.json { head :no_content }
       else
         format.html { render action: 'edit' }
