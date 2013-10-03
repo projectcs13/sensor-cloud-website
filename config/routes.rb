57c017c5a689781770ddf9052f7c2ac8dffc2c1d
Quentin Bahers <quentin.bahers@gmail.com>
2013-10-03 16:48:01 +0200

    Finish user signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 SensorCloud::Application.routes.draw do
-  get "users/new"
-
+	resources :users
   root  'static_pages#home'
   match '/signup',    to: 'users#new',            via: 'get'
   match '/help',      to: 'static_pages#help',    via: 'get'
