2486174001850fc859635e109c76c743c3a46aff
Mårten Blomberg <martenblomberg@gmail.com>
2013-10-04 13:54:32 +0200

    Add deps in gemfile, Modify db-schema

--- db/schema.rb
+++ db/schema.rb
@@ -12,8 +12,6 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema.define(version: 20131003140201) do
-# ActiveRecord::Schema.define(version: 20131003083021) do
-
 
   create_table "resources", force: true do |t|
     t.string   "owner"
