e5ddfdc1bbca0242264f881ab068719b2699716a
Quentin Bahers <quentin.bahers@gmail.com>
2013-10-07 14:27:32 +0200

    Finish sign in

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -38,6 +38,11 @@ describe "Authentication" do
 			it { should have_link('Profile', href: user_path(user)) }
 			it { should have_link('Sign out', href: signout_path) }
 			it { should_not have_link('Sign in', href: signin_path) }
+
+			describe "followed by signout" do
+				before { click_link "Sign out" }
+				it { should have_link('Sign in') }
+			end
 		end
 	end
 end
