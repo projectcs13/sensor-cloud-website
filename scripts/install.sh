6639b629761eeaa4bfd6e477e8fa5e40e2505513
Tommy <tommy.mattsson@gmail.com>
2013-10-08 23:00:56 +0200

    Added some pretty printing for the install script.

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,35 +1,57 @@
 #!/bin/bash --login
 touch ~/.bash_profile
 
-echo "Download ruby version manager"
+echo "=============================================================================="
+echo "Downloading RVM"
+echo "=============================================================================="
 curl -L https://get.rvm.io | bash -s
 
-echo "Set rvm command and bash sources, needed for ruby"
+echo "=============================================================================="
+echo "Set RVM command for access through bash shell"
+echo "=============================================================================="
 grep -q -e '[[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm"' ~/.bash_profile || echo '[[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm"' >> ~/.bash_profile
 
+echo "=============================================================================="
 echo "Set bash sources, needed for ruby"
+echo "=============================================================================="
 grep -q -e 'source ~/.profile' ~/.bash_profile || echo 'source ~/.profile' >> ~/.bash_profile
 
+echo "=============================================================================="
 echo "Reload bash profile"
+echo "=============================================================================="
 source ~/.bash_profile
 
-echo "Reload rvm"
+echo "=============================================================================="
+echo "Reload RVM"
+echo "=============================================================================="
 rvm reload
 
-echo "Get latest rvm version"
+echo "=============================================================================="
+echo "Download and install latest RVM version"
+echo "=============================================================================="
 rvm get stable
 
-echo "Get ruby requirements"
+echo "=============================================================================="
+echo "Download and install any Ruby requirements"
+echo "=============================================================================="
 rvm requirements
 
-echo "Install ruby 2.0.0"
+echo "=============================================================================="
+echo "Install Ruby 2.0.0"
+echo "=============================================================================="
 rvm install 2.0.0 --with-openssl-dir=$HOME/.rvm/usr
 
+echo "=============================================================================="
 echo "Specify which rails version to use"
+echo "=============================================================================="
 rvm use 2.0.0@railstutorial_rails_4_0 --create --default
 
-echo "Download all the gems specified in the Gemfile except the ones in the 'production' group"
+echo "=============================================================================="
+echo "Download and install all gems except the 'production' group"
+echo "=============================================================================="
 bundle install --without production
 
-echo "Create the database"
+echo "=============================================================================="
+echo "Creates the database to be used for Ruby on Rails"
+echo "=============================================================================="
 bundle exec rake db:migrate
