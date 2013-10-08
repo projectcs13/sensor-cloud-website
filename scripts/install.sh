f81b7595fcebdcd7a851338b81d64c467eade662
Tommy <tommy.mattsson@gmail.com>
2013-10-08 22:18:58 +0200

    Fixed an issue with the installation script

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,32 +1,32 @@
 #!/bin/bash --login
 touch ~/.bash_profile
 
-# Download ruby
+echo "Download ruby version manager"
 curl -L https://get.rvm.io | bash -s
 
-# Set rvm commandbash sources, needed for ruby
+echo "Set rvm command and bash sources, needed for ruby"
 grep -q -e '[[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm"' ~/.bash_profile || echo '[[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm"' >> ~/.bash_profile
 
-# Set bash sources, needed for ruby
+echo "Set bash sources, needed for ruby"
 grep -q -e 'source ~/.profile' ~/.bash_profile || echo 'source ~/.profile' >> ~/.bash_profile
 
-# Reload bash profile
+echo "Reload bash profile"
 source ~/.bash_profile
 
-# Get latest rvm version
+echo "Get latest rvm version"
 rvm get stable
 
-# Get ruby requirements
+echo "Get ruby requirements"
 rvm requirements
 
-# Install ruby 2.0.0
-rvm install 2.0.0 --with-open-ssl-dir=$HOME/.rvm/usr
+echo "Install ruby 2.0.0"
+rvm install 2.0.0 --with-openssl-dir=$HOME/.rvm/usr
 
-# Specify which rails version to use
+echo "Specify which rails version to use"
 rvm use 2.0.0@railstutorial_rails_4_0 --create --default
 
-# Download all the gems (libraries) specified in the Gemfile except the ones in the 'production' group
+echo "Download all the gems (libraries) specified in the Gemfile except the ones in the 'production' group"
 bundle install --without production
 
-# Create the database
+echo "Create the database"
 bundle exec rake db:migrate
