a7bceb360f5c624b6fb0245377d30bc447af4939
Alberto Blazquez <hola@albertoblazquez.net>
2013-10-14 16:10:37 +0200

    Change model relationships

--- app/models/stream.rb
+++ app/models/stream.rb
@@ -1,5 +1,7 @@
 class Stream < ActiveRecord::Base
   belongs_to :resource
+  belongs_to :group
+
   validates :name, presence: true
 	validates :resource_id, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,12 +3,13 @@ class User < ActiveRecord::Base
 	before_create :create_remember_token
 	validates :name,  presence: true, length: { maximum:50 }
 	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  	validates :email, presence: true, 
+  	validates :email, presence: true,
 										format: { with: VALID_EMAIL_REGEX },
 										uniqueness: { case_sensitive: false }
 	has_secure_password
 	validates :password, length: { minimum: 6 }
-	has_many :streams
+
+	has_many :groups
 
 	def User.new_remember_token
 		SecureRandom.urlsafe_base64
@@ -19,7 +20,6 @@ class User < ActiveRecord::Base
 	end
 
 	private
-
 		def create_remember_token
 			self.remember_token = User.encrypt(User.new_remember_token)
 		end
