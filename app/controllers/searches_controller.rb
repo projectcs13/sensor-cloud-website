6e82c5adecae51fa1466c34d0272e863efd42288
Quentin Bahers <quentin.bahers@gmail.com>
2013-11-12 16:31:21 +0100

    Implement sorting in search (work in progress)

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,30 +1,41 @@
 class SearchesController < ApplicationController
 
-  def index
-    logger.debug "Query: #{params}"
+  def show
+  end
 
-    if params['searches'] == ''
-      redirect_to root_path
-    else
+	def create
+		if params['search']['query'] == ''
+			redirect_to root_path
+		else
 
       conn = Faraday.new(:url => "#{CONF['API_URL']}") do |faraday|
-          faraday.request  :url_encoded             # form-encode POST params
-          faraday.response :logger                  # log requests to STDOUT
-          faraday.adapter  Faraday.default_adapter  # make requests with Net::HTTP
+      	faraday.request  :url_encoded             # form-encode POST params
+        faraday.response :logger                  # log requests to STDOUT
+        faraday.adapter  Faraday.default_adapter  # make requests with Net::HTTP
       end
+
       res = conn.post do |req|
         req.url '/_search'
         req.headers['Content-Type'] = 'application/json'
-        req.body = '{"query" : {"query_string" : { "query" : "' + params['searches'] + '"}}}'
-        logger.debug "#{req.body}"
+        #req.body = '{"query" : {"query_string" : { "query" : "' + params['search']['query'] + '"}}}'
+	if params['search']['sort_by'].nil?
+        	req.body = '{"sort": [{}], "query" : {"query_string" : { "query" : "' + params['search']['query'] + '"}}}'
+        	logger.debug "#{req.body}"
+	else
+		req.body = '{"sort": [{"' + params['search']['sort_by'] + '":"asc"}], "query" : {"query_string" : { "query" : "' + params['search']['query'] + '"}}}'
+        	logger.debug "#{req.body}"
+	end
       end
-      logger.debug "#{res.body}"
+logger.debug "#{res.body}"
       json = JSON.parse(res.body)
       @streams = json['streams']['hits']['hits']
       @users = json['users']['hits']['hits']
       @count_streams = json['streams']['hits']['total']
       @count_users = json['users']['hits']['total']
       @count_all = json['streams']['hits']['total'] + json['users']['hits']['total']
-    end
-  end
+	@query = params['search']['query']
+
+		render :action => 'show'
+	end
+	end
 end
