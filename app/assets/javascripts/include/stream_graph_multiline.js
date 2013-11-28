9b4f23f41334629ef8b3faf92899d12c219ce403
Mårten Blomberg <martenblomberg@gmail.com>
2013-11-28 16:38:30 +0100

    first draft of search view

--- app/assets/javascripts/include/stream_graph_multiline.js
+++ app/assets/javascripts/include/stream_graph_multiline.js
@@ -4,21 +4,22 @@ function streamGraphMultiLine () {
     margin2 = {top: 40, right: 20, bottom: 20, left: 50},
     width = 760,
     height = 500,
-    height2 = 200,
+    height2 = 100,
     xScale = d3.time.scale(),
     yScale = d3.scale.linear(),
     xScale2 = d3.time.scale(),
     yScale2 = d3.scale.linear(),
     xAxis = d3.svg.axis().scale(xScale).orient("bottom").tickSize(0, 0),
     yAxis = d3.svg.axis().scale(yScale).orient("left").tickSize(0).ticks(5),
-    xAxis2 = d3.svg.axis().scale(xScale2).orient("bottom").tickSize(0, 0);
+    xAxis2 = d3.svg.axis().scale(xScale2).orient("bottom").tickSize(0, 0).ticks(4);
 
   /* Variables for the graph and data */
   var  parseDate = d3.time.format("%Y-%m-%dT%H:%M:%S.%L").parse,
-    xValue = function(d) { return d.timestamp; },
+    xValue = function(d) { return parseDate(d.timestamp); },
     yValue = function(d) { return d.value; },
     colors = ["#00ff00", "#000000", "#ff0000", "#0000ff", "#342432", "#237282"],
     line = d3.svg.line().x(X).y(Y).interpolate("monotone"),
+    line2 = d3.svg.line().x(function(d) {return xScale2(parseDate(d.timestamp)); }).y(function(d) { return yScale2(d.value); }),
     real_data = function(d) { return d.data; },
     datasets = [];
     
@@ -26,24 +27,25 @@ function streamGraphMultiLine () {
     selection.each(function(data) {
 
       /* Setting up the domain and range for the axis */
-      var x_domain = d3.extent(data[3].data, function(d) { return d.timestamp; });
-      var y_domain = d3.extent(data[3].data, function(d) { return d.value; })
-      console.log(data);
-      data.forEach(function(d){
-        if(d.length > 0){
-          tempx_extent = d3.extent(d.data, function(d) { return d.timestamp; });
-          tempy_extent = d3.extent(d.data, function(d) { return d.value; });
-          x_domain[0] = d3.min(x_domain[0], tempx_extent[0]);
-          x_domain[1] = d3.max(x_domain[1], tempx_extent[1]);
-          y_domain[0] = d3.min(y_domain[0], tempy_extent[0]);
-          y_domain[1] = d3.max(y_domain[1], tempy_extent[1]);
+      var x_domain = d3.extent(data[0].data, function(d) { return parseDate(d.timestamp); });
+      var y_domain = d3.extent(data[0].data, function(d) { return d.value; })
+      for(var i = 0; i < data.length; i++){
+        var d = data[i];
+        if(d.data.length > 1){
+          var tempx_extent = d3.extent(d.data, function(d) { return parseDate(d.timestamp); });
+          var tempy_extent = d3.extent(d.data, function(d) { return d.value; });
+          y_domain[0] = Math.min(tempy_extent[0], y_domain[0]);
+          y_domain[1] = Math.max(tempy_extent[1], y_domain[1]);
+          x_domain[0] = Math.min(tempx_extent[0], x_domain[0]);
+          x_domain[1] = Math.max(tempx_extent[1], x_domain[1]);
         }
-      });
+      };
       xScale.domain(x_domain).range([0, width - margin.left - margin.right]);
       yScale.domain(y_domain).range([height - margin.top - margin.bottom, 0]);
       xScale2.domain(xScale.domain()).range(xScale.range());
-      yScale2.domain(yScale.domain()).range(yScale.range());
-
+      yScale2.domain(yScale.domain()).range([height2 - margin.top - margin.bottom, 0]);
+      console.log("x_domain:" + x_domain);
+      console.log("y_domain:" + y_domain);
       // Create brush
       var brush = d3.svg.brush()
                     .x(xScale2)
@@ -52,19 +54,20 @@ function streamGraphMultiLine () {
       var svg = d3.select(this).selectAll("svg").data([data]);
       // Otherwise, create the skeletal chart.
       var svgEnter = svg.enter().append("svg")
-      var focus = svgEnter.append("g");
-      var context = svgEnter.append("g");
+      var focus = svgEnter.append("g").attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+      var context = svgEnter.append("g").attr("transform", "translate(" + margin.left + "," + height + ")");
           focus.append("g").attr("class", "x axis");
           focus.append("g").attr("class", "y axis");
-          focus.attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-          context.append("g").attr("class", "x axis");
-          context.attr("transform", "translate(" + margin2.left + "," + margin2.top + ")");
-          context.append("g").attr("class", "x brush")
+          focus.append("g").attr("class", "lines");
+          context.append("g").attr("class", "lines");
+          context.append("g").attr("transform", "translate(0, 100)").attr("class", "x axis");
+          context.append("g")
+                  .attr("class", "x brush")
                   .call(brush)
                   .selectAll("rect")
                   .attr("opacity", 0.1)
                   .attr("y", -6)
-                  .attr("height", height2 + 7);
+                  .attr("height", height2);
 
       // Update the outer dimensions.
       svg .attr("width", width)
@@ -72,21 +75,29 @@ function streamGraphMultiLine () {
 
 
       // Update the inner dimensions.
-
-      var streams = focus.selectAll(".stream-line").data(data);
-
-      streams.attr("id", function(d){return d.stream_id});
-      streams.datum(real_data).attr("d", line);
+      var streams = focus.select(".lines").selectAll(".stream-line").data(data, function(d){return d.stream_id;});
+      var lines = streams.datum(function(d){return d.data;}).attr("d", line);
       
-      var lines = streams
+      streams
                   .enter()
                   .append("path")
                   .attr("class", "stream-line")
                   .attr("id", function(d){return d.stream_id})
                   .attr("stroke", function(d, i){ return colors[i]; })
-                  .datum(function(d){return d.data})
+                  .datum(function(d){return d.data;})
                   .attr("d", line);
 
+      var streams2 = context.select(".lines").selectAll(".stream-line").data(data, function(d){return d.stream_id;});
+      var lines2 = streams2.datum(function(d){return d.data;}).attr("d", line2);
+      
+      streams2
+                  .enter()
+                  .append("path")
+                  .attr("class", "stream-line")
+                  .attr("id", function(d){return d.stream_id})
+                  .attr("stroke", function(d, i){ return colors[i]; })
+                  .datum(function(d){return d.data;})
+                  .attr("d", line2);
           
       // Update the x-axis.
       focus.select(".x.axis")
@@ -101,20 +112,17 @@ function streamGraphMultiLine () {
           .call(xAxis2);
 
       function brushed() {
-        console.log("helloo...");
         xScale.domain(brush.empty() ? xScale2.domain() : brush.extent());
-        streams.attr("d", line);
         focus.select(".x.axis").call(xAxis);
         focus.select(".y.axis").call(yAxis);
+        lines.attr("d", line);
       }
-
-
     });
   }
 
   // The x-accessor for the path generator; xScale xValue.
   function X(d) {
-    return xScale(d.timestamp);
+    return xScale(parseDate(d.timestamp));
   }
 
   // The x-accessor for the path generator; yScale yValue.
--- app/assets/javascripts/searches.js.coffee
+++ app/assets/javascripts/searches.js.coffee
@@ -2,60 +2,72 @@
 //= require 'include/stream_graph_multiline'
 
 $ ->
-	graphWidth = $(".search-graph").width()
-	#console.log graphWidth
-	test_graph = streamGraphMultiLine().width(1000).height(500)
-	search_graph = searchGraph().width(1000).margin({top:0, right:0, left:0, bottom:0})
-	search_params = "?stream_id="
-	for graph in $(".search-graph")
-		search_params += graph.dataset.stream + ','
-	search_params = search_params.slice(0, search_params.length-1)
-	res = $.get '/history' + search_params
-	res.done (result) ->
-		datalist = result.history
-		for graph in datalist
-			d3.select(".search-graph#id"+graph.stream_id).datum(graph.data).call(search_graph)
-		d3.select("#test-multiline").datum(datalist).call(test_graph)
-		
-	res.fail (e, data) ->
-		console.log e
+  graphColors = ["#aabbcc", "#a21bc4", "#8bbbbc", "#5ab1cc", "#a3b1cc", "#aabbff", "#a124cc"]
+  graphWidth = $(".search-graph").width()
+  #console.log graphWidth
+  graphData = []
+  nr = 0
+  test_graph = streamGraphMultiLine().width(700).height(500)
+  search_graph = searchGraph().width(1000).margin({top:0, right:0, left:0, bottom:0})
+  search_params = "?stream_id="
+  for graph in $('#streams .search-result')
+    $(graph).on "click", (event) ->
+      stream_id = this.dataset.streamid
+      console.log $(this)
+      res = $.get '/datapoints/' + stream_id
+      res.done (result) ->
+        result['stream_id'] = stream_id
+        graphData.push result
+        $("#test-multiline svg").remove()
+        d3.select("#test-multiline").datum(graphData).call(test_graph)
+        console.log result
+      res.fail (e, data) ->
+        console.log e
 
-	# Setting up the slider
-	$("#slider-range").slider 
-		range: true,
-		min: 0,
-		max: 10,
-		values: [ 0, 5 ],
-		slide: ( event, ui ) -> 
-			$( "#min_val" ).val ui.values[ 0 ]
-			$( "#max_val" ).val ui.values[ 1 ]
+  
 
-	#$( "#slider-range" ).css("width","11em")
-	#map
-	mapDiv = document.getElementById('map-canvas')
-	#console.log mapDiv
-	map = new google.maps.Map(mapDiv, {
-    center: new google.maps.LatLng(59, 18),
-    zoom: 8,
-    mapTypeId: google.maps.MapTypeId.ROADMAP,
-    disableDefaultUI: true
+
+  # Setting up the slider
+  $("#slider-range").slider 
+   range: true,
+   min: 0,
+   max: 10,
+   values: [ 0, 5 ],
+   slide: ( event, ui ) -> 
+     $( "#min_val" ).val ui.values[ 0 ]
+     $( "#max_val" ).val ui.values[ 1 ]
+
+  #$( "#slider-range" ).css("width","11em")
+  #map
+  mapDiv = document.getElementById('map-canvas')
+  mapWidth = $(".right-side-content").width()
+  mapHeight = 500
+  console.log mapWidth
+  $('#map-canvas').width(mapWidth)
+  $('#map-canvas').height(mapHeight)
+  #console.log mapDiv
+  map = new google.maps.Map(mapDiv, {
+      center: new google.maps.LatLng(59, 18),
+      zoom: 8,
+      mapTypeId: google.maps.MapTypeId.ROADMAP,
+      disableDefaultUI: true
     })
 
-	bounds = new google.maps.LatLngBounds()
-	
-	$('#streams .search-result').each (i, elem) ->
-  		location = $(elem).data('location')
-  		if location != " "
-  			console.log location
-  			loc = location.split ","
-  			lon = loc[0]
-  			lat = loc[1]
-  			pos = new google.maps.LatLng(lon, lat)
-  			marker = new google.maps.Marker({position: pos,map: map,title:"Hello World!"})
-  			bounds.extend(pos)
-  	map.fitBounds(bounds);
+  bounds = new google.maps.LatLngBounds()
+  
+  $('#streams .search-result').each (i, elem) ->
+     location = $(elem).data('location')
+     if location != " "
+       console.log location
+       loc = location.split ","
+       lon = loc[0]
+       lat = loc[1]
+       pos = new google.maps.LatLng(lon, lat)
+       marker = new google.maps.Marker({position: pos,map: map,title:"Hello World!"})
+       bounds.extend(pos)
+    map.fitBounds(bounds);
 
-	#console.log searchresults
-	#for result in $("#stream-result > li")
-	#	console.log result
+  #console.log searchresults
+  #for result in $("#stream-result > li")
+  #  console.log result
 
