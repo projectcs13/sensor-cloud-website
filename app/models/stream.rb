73a4e31716aa50bf29bc95a74345bf28224665bb
Alberto Blazquez <hola@albertoblazquez.net>
2013-12-10 13:03:12 +0100

    Delete commented REST functions in the model

--- app/models/stream.rb
+++ app/models/stream.rb
@@ -15,36 +15,4 @@ class Stream
   collection_path "streams"
   include_root_in_json false
   parse_root_in_json :streams, format: :active_model_serializers
-
-=begin
-  def post uid
-    url = "#{CONF['API_URL']}/users/#{uid.to_s}/streams/"
-    send_data(:post, url)
-  end
-
-  def put uid, sid
-    url = "#{CONF['API_URL']}/users/#{uid.to_s}/streams/#{sid.to_s}"
-    send_data(:put, url)
-  end
-
-  private
-    def send_data(method, url)
-      new_connection
-
-      @conn.send(method) do |req|
-        req.url url
-        req.headers['Content-Type'] = 'application/json'
-        req.body = @attributes.to_json
-      end
-    end
-
-    def new_connection
-      @conn = Faraday.new(:url => "#{CONF['API_URL']}") do |faraday|
-        faraday.request  :url_encoded             # form-encode POST params
-        faraday.response :logger                  # log requests to STDOUT
-        faraday.adapter  Faraday.default_adapter  # make requests with Net::HTTP
-      end
-    end
-=end
-
 end
