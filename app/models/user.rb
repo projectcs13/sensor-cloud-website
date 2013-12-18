943bc784352576ede0f1e98e56306935d8d443fc
tomas <tomas@tomas>
2013-12-18 10:21:47 +0100

    On the way

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,8 @@
 class User < ActiveRecord::Base
 	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-	validates :username,  presence: true, length: { maximum:50 }, uniqueness: { case_sensitive: false }
-  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
+	VAILD_USERNAME_REGEX = /[\w]+/
+	validates :username,  presence: true, format: { with: VAILD_USERNAME_REGEX }, length: { maximum:50 }, uniqueness: { case_sensitive: false }
+  	validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
 	has_secure_password
 	validates :password, length: { minimum: 6 }
 	validates :description, length: { maximum:500 }
