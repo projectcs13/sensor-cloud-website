74005e8a3a10a71bc2e381d1f09c761e6bdbb2bc
Alberto Blazquez <hola@albertoblazquez.net>
2013-12-19 13:37:41 +0100

    Save and keep users without a password in the DB

--- config/initializers/user_db_population.rb
+++ config/initializers/user_db_population.rb
@@ -4,9 +4,19 @@ unless SensorCloud.rake?
 	unless users.nil?
 		users.each do | user |
 			['id', 'notifications', 'rankings', 'subscriptions', 'triggers'].each { |attr| user.delete attr }
+
+			## TODO Fix this security issue
+			user['password'] = "password" if user['password'] == ""
+			## END TODO
+
 			user['password_confirmation'] = user['password']
+
 			newuser = User.create user
-	  	if newuser.save then puts "Saved user with username\n#{newuser.attributes}" end
+	  	if newuser.save
+	  		puts "Saved user with username\n#{newuser.attributes}"
+	  	else
+	  		puts "Not saved with username\n#{newuser.attributes}"
+	  	end
   	end
 	end
 end
