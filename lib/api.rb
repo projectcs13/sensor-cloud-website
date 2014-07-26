c48f5b8da800e01721c99b952d30480b1ce3fafd
Alberto Blazquez <hola@albertoblazquez.net>
2014-07-26 17:14:38 +0200

    Refactor due to session[:token] is unreachable

--- lib/api.rb
+++ lib/api.rb
@@ -1,73 +1,74 @@
 require 'json'
 
-module Api
-	class << self
+# module Api
+# 	class << self
+class Api
 
-		def authenticate
-			res = post "/users/_auth/", {}, ""
-			res["body"]
-		end
-
-		def get(url)
-			url = append_param url, session[:token], "access_token"
-			res = connect.get url
-			parse_JSON_response res
-		end
-
-		def get_frontend(url)
-			url = append_param url, REFRESH_TOKEN, "refresh_token"
-			res = connect.get url
-			parse_JSON_response res
-		end
-
-		def post(url, body)
-			res = make :post, url, body, session[:token]
-			parse_JSON_response res
-		end
-
-		def post_frontend(url, body)
-			res = make :post, url, body, REFRESH_TOKEN
-			parse_JSON_response res
-		end
-
-		def put(url, body)
-			res = make :put, url, body, session[:token]
-		  parse_JSON_response res
-		end
-
-		def delete(url, body)
-			res = make :delete, url, body, session[:token]
-			parse_JSON_response res
-		end
-
-		private
-			def connect
-				conn = Faraday.new(:url => "#{CONF['API_URL']}") do |faraday|
-				  faraday.request  :url_encoded             # form-encode POST params
-				  faraday.response :logger                  # log requests to STDOUT
-				  faraday.adapter  Faraday.default_adapter  # make requests with Net::HTTP
-				end
-			end
-
-			def make(method, url, body, token)
-				res = connect.send method do |req|
-				  req.url url
-				  req.headers['Content-Type'] = 'application/json'
-				  req.headers['Access-Token'] = token
-				  req.body = body.to_json
-				end
-			end
-
-			def parse_JSON_response(res)
-				resp = res.to_hash
-				resp[:body] = JSON.parse resp[:body]
-				resp.stringify_keys!
-			end
-
-			def append_param(url, token, tokentype)
-				char = if url.include? "?" then "&" else "?" end
-				url + "#{char}#{tokentype}=" + token
-			end
-
+	def self.authenticate
+		res = post "/users/_auth/", {}, ""
+		res["body"]
 	end
+
+	def self.get(url, token)
+		url = append_param url, token, "access_token"
+		res = connect.get url
+		parse_JSON_response res
+	end
+
+	def self.get_frontend(url)
+		url = append_param url, REFRESH_TOKEN, "refresh_token"
+		res = connect.get url
+		parse_JSON_response res
+	end
+
+	def self.post(url, body, token)
+		res = make :post, url, body, token
+		parse_JSON_response res
+	end
+
+	def self.post_frontend(url, body)
+		res = make :post, url, body, REFRESH_TOKEN
+		parse_JSON_response res
+	end
+
+	def self.put(url, body, token)
+		res = make :put, url, body, token
+	  parse_JSON_response res
+	end
+
+	def self.delete(url, body, token)
+		res = make :delete, url, body, token
+		parse_JSON_response res
+	end
+
+	private
+		def self.connect
+			conn = Faraday.new(:url => "#{CONF['API_URL']}") do |faraday|
+			  faraday.request  :url_encoded             # form-encode POST params
+			  faraday.response :logger                  # log requests to STDOUT
+			  faraday.adapter  Faraday.default_adapter  # make requests with Net::HTTP
+			end
+		end
+
+		def self.make(method, url, body, token)
+			res = connect.send method do |req|
+			  req.url url
+			  req.headers['Content-Type'] = 'application/json'
+			  req.headers['Access-Token'] = token
+			  req.body = body.to_json
+			end
+		end
+
+		def self.parse_JSON_response(res)
+			resp = res.to_hash
+			resp[:body] = JSON.parse resp[:body]
+			resp.stringify_keys!
+		end
+
+		def self.append_param(url, token, tokentype)
+			char = if url.include? "?" then "&" else "?" end
+			url + "#{char}#{tokentype}=" + token
+		end
+
+	# end
 end
