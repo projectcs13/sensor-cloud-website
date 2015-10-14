cc7894fb3d0a108da06755cb6d9e8b145a7e54e7
Konstantinos Vandikas <konstantinos.vandikas@ericsson.com>
2015-10-14 13:16:35 +0200

    use apt in order to install ruby

--- scripts/install.sh
+++ scripts/install.sh
@@ -13,59 +13,27 @@ install_boot_script() {  # $1 is the script
 }
 
 echo "=============================================================================="
-echo "Downloading RVM"
-echo "=============================================================================="
-curl -L https://get.rvm.io | bash -s
-
-#echo "=============================================================================="
-#echo "Set RVM command for access through bash shell"
-#echo "=============================================================================="
-grep -q -e '[[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm"' ~/.bash_profile || echo '[[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm"' >> ~/.bash_profile
-
-#echo "=============================================================================="
-#echo "Set bash sources, needed for ruby"
-#echo "=============================================================================="
-grep -q -e 'source ~/.profile' ~/.bash_profile || echo 'source ~/.profile' >> ~/.bash_profile
-
-echo "=============================================================================="
-echo "Reload bash profile"
-echo "=============================================================================="
-source ~/.bash_profile
-
-echo "=============================================================================="
-echo "Reload RVM"
-echo "=============================================================================="
-rvm reload
-
-echo "=============================================================================="
-echo "Download and install latest RVM version"
-echo "=============================================================================="
-rvm get stable --auto-dotfiles
-
-echo "=============================================================================="
-echo "Reload bash profile"
-echo "=============================================================================="
-source ~/.bash_profile
-
-echo "=============================================================================="
-echo "Download and install any Ruby requirements"
-echo "=============================================================================="
-rvm requirements
-
-echo "=============================================================================="
 echo "Install Ruby 2.0.0"
 echo "=============================================================================="
-rvm install 2.0.0 --with-openssl-dir=$HOME/.rvm/usr
-
-echo "=============================================================================="
-echo "Reinstall Ruby 2.0.0"
-echo "=============================================================================="
-rvm reinstall 2.0.0
+apt-get install -yq git software-properties-common curl wget make build-essential bison openssl libreadline6 libreadline6-dev curl git-core zlib1g zlib1g-dev libssl-dev libyaml-dev libxml2-dev autoconf libc6-dev ncurses-dev automake libtool ruby2.0 ruby2.0-dev libpq-dev libsqlite3-dev
 
 echo "=============================================================================="
 echo "Specify which rails version to use"
 echo "=============================================================================="
-rvm use 2.0.0@railstutorial_rails_4_0 --create --default
+# switch to ruby 2.0
+rm /usr/bin/ruby /usr/bin/gem /usr/bin/irb /usr/bin/rdoc /usr/bin/erb
+ln -s /usr/bin/ruby2.0 /usr/bin/ruby
+ln -s /usr/bin/gem2.0 /usr/bin/gem
+ln -s /usr/bin/irb2.0 /usr/bin/irb
+ln -s /usr/bin/rdoc2.0 /usr/bin/rdoc
+ln -s /usr/bin/erb2.0 /usr/bin/erb
+gem update --system
+gem pristine --all
+
+echo "=============================================================================="
+echo "Specify which rails version to use"
+echo "=============================================================================="
+gem install bundler
 
 echo "=============================================================================="
 echo "Download and install all gems except the 'production' group"
